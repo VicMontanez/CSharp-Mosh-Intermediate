94452cb31a6c0e58014d21998b619ea04782ee05
VicMontanez <v_montanez@hotmail.com>
2020-02-25 15:29:36 -0500

    created classes

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classes
+{
+
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+        }
+    }
+}
