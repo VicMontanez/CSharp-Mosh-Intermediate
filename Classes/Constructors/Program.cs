56ff78d0a15ef5dc1bc8e20b116e8d7e1306b09c
VicMontanez <v_montanez@hotmail.com>
2020-02-29 13:54:27 -0500

    created new class for order

--- Classes/Constructors/Program.cs
+++ Classes/Constructors/Program.cs
@@ -8,6 +8,10 @@ namespace Constructors
         static void Main(string[] args)
         {
             var customer = new Customer(1, "John");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
         }
