9266c3141c84373c8d2767396da95abf2cf0b172
VicMontanez <v_montanez@hotmail.com>
2020-02-28 21:06:36 -0500

    created customer class

--- Classes/Constructors/Program.cs
+++ Classes/Constructors/Program.cs
@@ -2,6 +2,11 @@
 
 namespace Constructors
 {
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+    }
     class Program
     {
         static void Main(string[] args)
