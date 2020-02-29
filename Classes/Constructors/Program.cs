5e77585ca483f6afca4f44ea393bdc9e0c3f9d96
VicMontanez <v_montanez@hotmail.com>
2020-02-29 13:39:24 -0500

    added parameters

--- Classes/Constructors/Program.cs
+++ Classes/Constructors/Program.cs
@@ -7,7 +7,7 @@ namespace Constructors
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
+            var customer = new Customer(1, "John");
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
         }
