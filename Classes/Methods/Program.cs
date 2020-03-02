31e27ec21ee9d9367f9111eddb65475aa1bf80db
VicMontanez <v_montanez@hotmail.com>
2020-03-01 19:57:30 -0500

    added try and catch

--- Classes/Methods/Program.cs
+++ Classes/Methods/Program.cs
@@ -7,12 +7,20 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            var point = new Point(10, 20);
-            point.Move(new Point(40, 60));
-            Console.WriteLine("Point is at ({0}, {1}", point.X, point.Y);
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                Console.WriteLine("Point is at ({0}, {1}", point.X, point.Y);
 
-            point.Move(100, 200);
-            Console.WriteLine("Point is at ({0}, {1}", point.X, point.Y);
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1}", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured");
+            }
+
         }
     }
 }
