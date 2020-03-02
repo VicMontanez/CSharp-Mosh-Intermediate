48c138d50e9bc6366b8b5093c9de42c1e3e8d26f
VicMontanez <v_montanez@hotmail.com>
2020-03-01 19:24:31 -0500

    created point class

---
+++ Classes/Methods/Point.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y - y;
+        }
+    }
+}
