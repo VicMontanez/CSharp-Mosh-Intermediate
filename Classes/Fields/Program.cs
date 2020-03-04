aa6b3cf209350763fa6c1a60bfbbd34976a71852
VicMontanez <v_montanez@hotmail.com>
2020-03-03 23:25:14 -0500

    made customer variable

--- Classes/Fields/Program.cs
+++ Classes/Fields/Program.cs
@@ -2,32 +2,17 @@
 
 namespace Fields
 {
-    public class Customer
-    {
-        public int Id;
-        public string Name;
-        public List<Order> Orders = new List<Order>();
 
-        public Customer(int id)
-        {
-            this.Id = id;
-        }
 
-        public Customer(int id, string name)
-        {
-            this.Name = name;
-        }
-    }
-
-    public class Order
-    {
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);
         }
     }
 }
