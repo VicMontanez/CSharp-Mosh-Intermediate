2969cbc1fa3a73ff060c4efb6dd75f8c60cee74f
VicMontanez <v_montanez@hotmail.com>
2020-03-03 23:07:34 -0500

    made customer class

--- Classes/Fields/Program.cs
+++ Classes/Fields/Program.cs
@@ -1,7 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Fields
 {
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+
+        }
+    }
+
+    public class Order
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
