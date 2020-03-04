e4ec7dd94767cd6423b6f5156f87e44e9be6fe67
VicMontanez <v_montanez@hotmail.com>
2020-03-03 23:11:06 -0500

    created second customer class

--- Classes/Fields/Program.cs
+++ Classes/Fields/Program.cs
@@ -8,9 +8,14 @@ namespace Fields
         public string Name;
         public List<Order> Orders;
 
-        public Customer()
+        public Customer(int id)
         {
+            this.Id = id;
+        }
 
+        public Customer(int id, string name)
+        {
+            this.Name = name;
         }
     }
 
