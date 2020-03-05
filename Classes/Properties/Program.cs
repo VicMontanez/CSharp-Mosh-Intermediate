072a26a4cbdb5609ea557dc966b85ddf57d79d36
VicMontanez <v_montanez@hotmail.com>
2020-03-04 21:29:56 -0500

    writeline

--- Classes/Properties/Program.cs
+++ Classes/Properties/Program.cs
@@ -2,27 +2,14 @@
 
 namespace Properties
 {
-    public class Person
-    {
-        public DateTime Birthdate { get; set; }
 
-        public int Age
-        {
-            get
-            {
-                var timeSpan = DateTime.Today - Birthdate;
-                var years = timeSpan.Days / 365;
-
-                return years;
-            }
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new Person();
+            person.Birthdate = new DateTime(1982, 1, 1);
+            Console.WriteLine(person.Age);
         }
     }
 }
