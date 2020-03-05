beac69e6f4b9da4d17c250fcf0b6d73a2c4f0808
VicMontanez <v_montanez@hotmail.com>
2020-03-05 18:52:04 -0500

    added methods

--- AssociationBetweenClasses/Inheritance/Program.cs
+++ AssociationBetweenClasses/Inheritance/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Inheritance
 {
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+
+        }
+
+        public void Duplicate()
+        {
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
