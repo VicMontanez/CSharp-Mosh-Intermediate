e32c768e63ed2800da3f52a04e5259ec75fc4157
VicMontanez <v_montanez@hotmail.com>
2020-03-04 20:13:53 -0500

    created person class

---
+++ Classes/Properties/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public DateTime Birthdate { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
