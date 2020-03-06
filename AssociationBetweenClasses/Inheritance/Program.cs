a6761e36aee19edb83e60c4fe935faf01a6a93ab
VicMontanez <v_montanez@hotmail.com>
2020-03-05 19:02:00 -0500

    added variable

--- AssociationBetweenClasses/Inheritance/Program.cs
+++ AssociationBetweenClasses/Inheritance/Program.cs
@@ -2,18 +2,13 @@
 
 namespace Inheritance
 {
-    public class Text : PresentationObject
-    {
-        public int FontSize { get; set; }
-        public string FontName { get; set; }
-
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
         }
     }
 }
