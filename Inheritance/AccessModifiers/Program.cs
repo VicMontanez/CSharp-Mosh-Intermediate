881cde9aa9257ca6e925f148305dfc7fd62a4487
VicMontanez <v_montanez@hotmail.com>
2020-03-09 18:13:56 -0400

    updated code

--- Inheritance/AccessModifiers/Program.cs
+++ Inheritance/AccessModifiers/Program.cs
@@ -1,33 +1,17 @@
 ﻿using System;
 
+using Amazon;
+
 namespace AccessModifiers
 {
-    public class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
 
-        public void Promote()
-        {
-            var rating = CalculateRating();
-            if (rating == 0)
-                Console.WriteLine("Promote to Level 1");
-            else
-                Console.WriteLine("Promooted to Level 2");
-        }
-
-        public int CalculateRating()
-        {
-            return 0;
-        }
-
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
             var customer = new Customer();
+            Amazon.RateCalculator calculator = new RateCalculator();
+
 
         }
     }
