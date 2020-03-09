1026c5e7d36e3541f058dd914f32e4d239134cd9
VicMontanez <v_montanez@hotmail.com>
2020-03-08 23:39:28 -0400

    added file

---
+++ Inheritance/Amazon/RateCalculator.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amazon
+{
+    class RateCalculator
+    {
+        internal int Calculate()
+        {
+            return 0;
+        }
+    }
+}
