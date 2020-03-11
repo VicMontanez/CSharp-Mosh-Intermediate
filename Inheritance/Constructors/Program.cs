b6ec11a549e306539f80c94c8f01c6012bbdb4c9
VicMontanez <v_montanez@hotmail.com>
2020-03-11 16:19:19 -0400

    created vehicle class

---
+++ Inheritance/Constructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
