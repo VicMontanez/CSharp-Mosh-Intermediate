740329d78a62c61c1b11e19ff2b83e94c071fbd4
VicMontanez <v_montanez@hotmail.com>
2020-03-11 19:49:48 -0400

    added registration number

--- Inheritance/Constructors/Vehicle.cs
+++ Inheritance/Constructors/Vehicle.cs
@@ -5,14 +5,16 @@ namespace Constructors
     public class Vehicle
     {
         private readonly string _registrationNumber;
-        public Vehicle()
-        {
-            Console.WriteLine("Vehicle is being initialized.");
-        }
+        // public Vehicle()
+        // {
+        //     Console.WriteLine("Vehicle is being initialized.");
+        // }
 
         public Vehicle(string registrationNumber)
         {
             _registrationNumber = registrationNumber;
+
+            Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
         }
     }
 }
