887a55630f642efa144024d59d1d86f9f0f602d2
VicMontanez <v_montanez@hotmail.com>
2020-03-11 19:40:24 -0400

    created vehicle constructor

--- Inheritance/Constructors/Vehicle.cs
+++ Inheritance/Constructors/Vehicle.cs
@@ -4,9 +4,15 @@ namespace Constructors
 {
     public class Vehicle
     {
+        private readonly string _registrationNumber;
         public Vehicle()
         {
             Console.WriteLine("Vehicle is being initialized.");
+        }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
         }
     }
 }
