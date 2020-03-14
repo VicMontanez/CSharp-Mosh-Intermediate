b0dc16de83bc312361655b8801d7dc15e8f268a9
VicMontanez <v_montanez@hotmail.com>
2020-03-13 22:45:57 -0400

    addeadded file

---
+++ AssociationBetweenClasses/Inheritance/Person.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Inheritance
+{
+
+    public class Person
+    {
+
+        public string Firstname;
+        public string Lastname;
+        private int age;
+
+
+
+    }
+
+}
